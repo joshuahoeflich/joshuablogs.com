638382f48f8fd6b5b65046a2665a9f56b6c899c5
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-01-05 16:56:10 -0600

    Adds npm install to build script

--- compile.bash
+++ compile.bash
@@ -4,11 +4,13 @@ set -e
 PROJECT_ROOT="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 # Blog {{{
+npm install --prefix "$PROJECT_ROOT"/build/bundle
 npm run build --prefix "$PROJECT_ROOT"/build/bundle
 node "$PROJECT_ROOT"/build/bundle/dist/index.js
 # }}}
 
 # joke-generator {{{
+npm install --prefix "$PROJECT_ROOT"/apps/joke-generator/
 npm run build --prefix "$PROJECT_ROOT"/apps/joke-generator/
 mkdir -p "$PROJECT_ROOT"/apps/apps/joke-generator
 cp -r "$PROJECT_ROOT"/apps/joke-generator/build/* "$PROJECT_ROOT"/apps/apps/joke-generator
