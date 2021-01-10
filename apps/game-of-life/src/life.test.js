afde9bb0d348f7b60d7535fe0d14301566a08484
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-01-10 11:16:23 -0600

    Initialize ClojureScript code

--- apps/game-of-life/src/life.test.js
+++
@@ -1,5 +0,0 @@
-describe("life.js", () => {
-  test("Does a thing", () => {
-    expect(2 + 2).toEqual(4);
-  });
-});
---
+++ apps/game-of-life/src/main/life/frontend/app.cljs
@@ -0,0 +1,4 @@
+(ns life.frontend.app)
+
+(defn init []
+  (println (= {:x 3 :y 4} {:x 4 :y 3})))
