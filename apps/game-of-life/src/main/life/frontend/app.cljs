9315714f4930671798a40627e42140e70b4b8b6f
Joshua Hoeflich <josh@joshuahoeflich.com>
2021-01-10 20:05:46 -0600

    First draft at tick

--- apps/game-of-life/src/main/life/frontend/app.cljs
+++ apps/game-of-life/src/main/life/frontend/app.cljs
@@ -1,5 +1,30 @@
 (ns life.frontend.app)
 
+(defn neighbors [cell]
+  (let [{row :row col :col} cell]
+    [{:row (inc row) :col       col}
+     {:row row       :col (inc col)}
+     {:row (dec row) :col       col}
+     {:row row       :col (dec col)}
+     {:row (inc row) :col (inc col)}
+     {:row (dec row) :col (dec col)}
+     {:row (inc row) :col (dec col)}
+     {:row (dec row) :col (inc col)}]))
+
+(defn tick [board]
+  (let [cells (distinct (mapcat neighbors board))
+        is-living? (partial contains? board)
+        is-dead? (complement is-living?)
+        live-neighbors (fn [cell] (count (filter is-living? (neighbors cell))))
+        in-new-board?
+        (fn [cell]
+          (let [neighbor-count (live-neighbors cell)]
+            (if (is-living? cell)
+              (or (= neighbor-count 2) (= neighbor-count 3))
+              (= neighbor-count 3))))
+        ]
+    (into #{} (filter in-new-board? cells))))
+
 (defn init []
   (let [board #{{:x 3 :y 4}}]
     (println (contains? board {:x 3 :y 4}))
--- apps/game-of-life/src/test/life/frontend/test.cljs
+++ apps/game-of-life/src/test/life/frontend/test.cljs
@@ -1,9 +1,42 @@
 (ns life.frontend.test
-  (:require [cljs.test :refer (deftest is run-tests)]))
+  (:require
+   [life.frontend.app :refer [tick neighbors]]
+   [clojure.test :refer [deftest is run-tests]]))
 
-(deftest a-failing-test
-  (is (= 1 2)))
+(deftest root-neighbors
+  (let [cell {:row 0 :col 0}]
+    (is (= (neighbors cell)
+           [
+            {:row 1 :col 0}
+            {:row 0 :col 1}
+            {:row -1 :col 0}
+            {:row 0 :col -1}
+            {:row 1 :col 1}
+            {:row -1 :col -1}
+            {:row 1 :col -1}
+            {:row -1 :col 1}
+            ]))))
+
+(deftest bi-block
+  (let [board #{{:row 0 :col 0}
+               {:row 1 :col 0}
+               {:row 0 :col 1}
+               {:row 1 :col 1}}]
+    (is (= (tick board) board))))
+
+(deftest blinker
+  (let [board #{{:row 1 :col 0}
+               {:row 1 :col 1}
+               {:row 1 :col 2}}]
+    (is (= (tick board) #{{:row 0 :col 1}
+                         {:row 1 :col 1}
+                         {:row 2 :col 1}}))))
+
+(deftest blinker-resets
+  (let [board #{{:row 1 :col 0}
+               {:row 1 :col 1}
+               {:row 1 :col 2}}]
+    (is (= board (tick (tick board))))))
 
 (defn init []
   (run-tests))
-
